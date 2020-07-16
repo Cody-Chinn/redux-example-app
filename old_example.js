2a6f33bff821ee226671e8aceb219c082559a34d
Cody Chinn <cchinn1@steelcase.com>
2020-07-16 15:59:59 -0400

    initial commit

---
+++ old_example.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+// STORE -> Globalized State
+
+// ACTION INCREMENT
+const increment = () => {
+  return {
+    name: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    name: 'INCREMENT'
+  }
+}
+
+// REDUCER 
+const counter = (state = 0, action) => {
+    switch(action.type){
+      case 'INCREMENT':
+        return state + 1;
+      case 'DECREMENT':
+        return state - 1;
+      default:
+        return state;
+    }
+}
+
+let store = createStore(counter)
+
+// Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
